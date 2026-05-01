95562ac1cd64bbbcf9b0da8fe68be6d357305cbb
Shriya Sree Aachi <shriyasreeaachi@Mac.lan>
2026-05-01 06:58:28 -0500

    finished phase2

--- src/controller/RobotController.cpp
+++ src/controller/RobotController.cpp
@@ -11,65 +11,75 @@
 #include <QPushButton>
 #include <QString>
 
-namespace {
-const QString savedRobotsFolder = "saved_robots";
+namespace
+{
+    const QString savedRobotsFolder = "saved_robots";
 
-QString savedRobotsPath(const QString& fileName = "") {
-    QDir().mkpath(savedRobotsFolder);
-    return fileName.isEmpty() ? savedRobotsFolder : savedRobotsFolder + "/" + fileName;
-}
+    QString savedRobotsPath(const QString &fileName = "")
+    {
+        QDir().mkpath(savedRobotsFolder);
+        return fileName.isEmpty() ? savedRobotsFolder : savedRobotsFolder + "/" + fileName;
+    }
 }
 
-RobotController::RobotController(Robot* model, RobotWindow* view)
+RobotController::RobotController(Robot *model, RobotWindow *view)
     : model(model), view(view) {}
 
-void RobotController::initialize() {
+void RobotController::initialize()
+{
     view->setController(this);
     connectButtons();
     refreshView();
 }
 
-void RobotController::moveUp() {
+void RobotController::moveUp()
+{
     model->moveUp();
     refreshView();
 }
 
-void RobotController::moveDown() {
+void RobotController::moveDown()
+{
     model->moveDown();
     refreshView();
 }
 
-void RobotController::moveLeft() {
+void RobotController::moveLeft()
+{
     model->moveLeft();
     refreshView();
 }
 
-void RobotController::moveRight() {
+void RobotController::moveRight()
+{
     model->moveRight();
     refreshView();
 }
 
-void RobotController::undo() {
+void RobotController::undo()
+{
     model->undo();
     refreshView();
 }
 
-void RobotController::redo() {
+void RobotController::redo()
+{
     model->redo();
     refreshView();
 }
 
-void RobotController::saveHistory() {
+void RobotController::saveHistory()
+{
     QString defaultFileName = QString::fromStdString(model->getName()) + ".robot";
 
     QString filePath = QFileDialog::getSaveFileName(
         view,
         "Save Robot History",
         savedRobotsPath(defaultFileName),
-        "Robot Files (*.robot)"
-    );
+        "Robot Files (*.robot)");
 
-    if (filePath.isEmpty()) {
+    if (filePath.isEmpty())
+    {
         return;
     }
 
@@ -77,15 +87,16 @@ void RobotController::saveHistory() {
     view->setStatusText("Saved " + QFileInfo(filePath).fileName());
 }
 
-void RobotController::loadHistory() {
+void RobotController::loadHistory()
+{
     QString filePath = QFileDialog::getOpenFileName(
         view,
         "Load Robot History",
         savedRobotsPath(),
-        "Robot Files (*.robot)"
-    );
+        "Robot Files (*.robot)");
 
-    if (filePath.isEmpty()) {
+    if (filePath.isEmpty())
+    {
         return;
     }
 
@@ -94,33 +105,40 @@ void RobotController::loadHistory() {
     view->setStatusText("Loaded " + QFileInfo(filePath).fileName());
 }
 
-bool RobotController::handleKeyPress(QKeyEvent* event) {
-    if (event->key() == Qt::Key_Up) {
+bool RobotController::handleKeyPress(QKeyEvent *event)
+{
+    if (event->key() == Qt::Key_W)
+    {
         moveUp();
         return true;
     }
 
-    if (event->key() == Qt::Key_Down) {
+    if (event->key() == Qt::Key_S)
+    {
         moveDown();
         return true;
     }
 
-    if (event->key() == Qt::Key_Left) {
+    if (event->key() == Qt::Key_A)
+    {
         moveLeft();
         return true;
     }
 
-    if (event->key() == Qt::Key_Right) {
+    if (event->key() == Qt::Key_D)
+    {
         moveRight();
         return true;
     }
 
-    if (event->key() == Qt::Key_Z && event->modifiers() == Qt::ControlModifier) {
+    if (event->key() == Qt::Key_Z && event->modifiers() == Qt::ControlModifier)
+    {
         undo();
         return true;
     }
 
-    if (event->key() == Qt::Key_Y && event->modifiers() == Qt::ControlModifier) {
+    if (event->key() == Qt::Key_Y && event->modifiers() == Qt::ControlModifier)
+    {
         redo();
         return true;
     }
@@ -128,7 +146,8 @@ bool RobotController::handleKeyPress(QKeyEvent* event) {
     return false;
 }
 
-void RobotController::refreshView() {
+void RobotController::refreshView()
+{
     view->setRobotPosition(model->getX(), model->getY());
     view->setUndoEnabled(model->canUndo());
     view->setRedoEnabled(model->canRedo());
@@ -138,13 +157,17 @@ void RobotController::refreshView() {
             .arg(model->getX())
             .arg(model->getY())
             .arg(model->getCurrentHistoryIndex() + 1)
-            .arg(static_cast<int>(model->getHistory().size()))
-    );
+            .arg(static_cast<int>(model->getHistory().size())));
 }
 
-void RobotController::connectButtons() {
-    QObject::connect(view->getUndoButton(), &QPushButton::clicked, [this]() { undo(); });
-    QObject::connect(view->getRedoButton(), &QPushButton::clicked, [this]() { redo(); });
-    QObject::connect(view->getSaveButton(), &QPushButton::clicked, [this]() { saveHistory(); });
-    QObject::connect(view->getLoadButton(), &QPushButton::clicked, [this]() { loadHistory(); });
+void RobotController::connectButtons()
+{
+    QObject::connect(view->getUndoButton(), &QPushButton::clicked, [this]()
+                     { undo(); });
+    QObject::connect(view->getRedoButton(), &QPushButton::clicked, [this]()
+                     { redo(); });
+    QObject::connect(view->getSaveButton(), &QPushButton::clicked, [this]()
+                     { saveHistory(); });
+    QObject::connect(view->getLoadButton(), &QPushButton::clicked, [this]()
+                     { loadHistory(); });
 }
